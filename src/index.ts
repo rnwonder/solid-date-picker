fa33387eb1302e6cfcc72116e5e1792492d83123
rnwonder <60509154+rnwonder@users.noreply.github.com>
2023-06-05 05:31:43 +0100

    export portal and popover

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { DatePickerGroup } from "./components/DatePickerGroup";
+export { CustomPortal as RnPortal } from "./components/CustomPortal";
 export { MonthSelector } from "./components/MonthSelector";
 export { YearSelector } from "./components/YearSelector";
 export { CalendarArea } from "./components/CalendarArea";
+export { Popover } from "./components/Popover";
+
 export { utils } from "./utils";
 export * from "./interface/general";
-import "./index.css"
-// import "./style.css";
+import "./index.css";
+
 const DatePicker = DatePickerGroup;
 export default DatePicker;
